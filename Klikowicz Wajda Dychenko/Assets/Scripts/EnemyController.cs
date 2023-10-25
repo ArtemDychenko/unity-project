2df18bc140975dfd06a60d195b45d2770c5540c3
Artem Dychenko <134294885+ArtemDychenko@users.noreply.github.com>
2023-10-25 22:57:17 +0200

    instrukcja 2

---
+++ Klikowicz Wajda Dychenko/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    private Animator animator;
+
+    private Rigidbody2D rigidBody;
+
+    private float startPositionX;
+
+  
+
+    [Range(0.01f, 20.0f)]
+    [SerializeField]
+    private float moveRange = 1.0f;
+
+    [Range(0.01f, 20.0f)]
+    [SerializeField]
+    private float moveSpeed = 0.1f;
+
+    private bool isMovingRight = false;
+    private bool isFacingRight = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        startPositionX = this.transform.position.x;
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void MoveRight()
+    {
+        transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+    }
+
+    private void MoveLeft()
+    {
+        transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            if (transform.position.y < other.gameObject.transform.position.y)
+            {
+                
+                animator.SetBool("isDead", true);
+                StartCoroutine(KillOnAnimationEnd());
+                Debug.Log("Killed by player");
+            }
+           
+
+        }
+
+    }
+
+
+    IEnumerator KillOnAnimationEnd()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("zdechlem");
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = transform.position.x;
+        if (isMovingRight)
+        {
+            if (posX < moveRange + startPositionX)
+            {
+                MoveRight();
+            } else
+            {
+                Flip();
+                isMovingRight = false;
+            }
+        }else
+        {
+            if (posX > -moveRange + startPositionX)
+            {
+                MoveLeft();
+            }
+            else
+            {
+                Flip();
+                isMovingRight = true;
+            }
+        }
+        
+
+    }
+}
---
+++ Klikowicz Wajda Dychenko/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement parameters")]
+    [Range(0.01f, 20.0f)]
+    [SerializeField]
+    private float moveSpeed = 0.1f;
+
+    [Range(0.01f, 20.0f)]
+    [SerializeField]
+    private float jumpForce = 6.0f;
+
+    private Rigidbody2D rigidBody;
+
+    private Animator animator;
+
+    private bool isFacingRight = true;
+
+    private int lives = 3;
+
+    private Vector2 startPosition;
+
+    private bool isWalking = false;
+
+    private int score = 0;
+
+    private int keysFound = 0;
+    private int keysNumber = 3;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    [Range(0.01f, 20.0f)]
+    [SerializeField]
+    private float rayLength = 1.4f;
+
+    // [Space( 10 )]
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();   
+        animator = GetComponent<Animator>();
+        startPosition = transform.position;
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+    bool isGrounded()
+    {
+        return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
+    }
+
+    void jump()
+    {
+        if (isGrounded())
+        {
+            rigidBody.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void Death()
+    {
+        lives--;
+        if (lives == 0)
+        {
+            Debug.Log("Koniec gry");
+            transform.Translate(0.0f, -1232132131.0f, 0.0f, Space.World);
+        }
+        else
+        {
+            Debug.Log(lives);
+            transform.position = startPosition;
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bonus"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.CompareTag("Enemy"))
+        {
+            if (transform.position.y > other.gameObject.transform.position.y)
+            {
+                score++;
+                Debug.Log("Killed an enemy");
+            }else
+            {
+                Death();
+                
+            }
+
+        }
+
+        if (other.CompareTag("Key"))
+        {
+            keysFound++;
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.CompareTag("Heart"))
+        {
+            lives++;
+            Debug.Log("Acquired live");
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.CompareTag("Exit") && keysFound == keysNumber)
+        {
+            Debug.Log("You WIn");
+        }
+
+        if (other.CompareTag("FallLevel"))
+        {
+            Death();
+        }
+
+    }
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         isWalking = false;
+
+        
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            jump();
+
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            isWalking = true;
+            if (isFacingRight == false)
+            {
+                Flip();
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            isWalking = true;
+            if (isFacingRight == true)
+            {
+                Flip();
+            }
+        }
+
+        animator.SetBool("isGrounded", isGrounded());
+        animator.SetBool("isWalking", isWalking);
+
+
+
+        // Debug.DrawRay(transform.position, rayLength * Vector3.down, Color.white, 1, false);
+    }
+
+}
